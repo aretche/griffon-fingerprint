a4fa84a9c7f607fadc68db3621a7a910b0d11cb7
Gabriel Arellano <arellanog@cent.gov.ar>
2017-12-04 13:12:09 -0300

    Ajusto el modelo para contener el lector, la última huella capturada y la última enrolada

--- griffon-app/models/io/github/aretche/griffonFingerprint/GriffonFingerprintModel.groovy
+++ griffon-app/models/io/github/aretche/griffonFingerprint/GriffonFingerprintModel.groovy
@@ -1,10 +1,21 @@
 package io.github.aretche.griffonFingerprint
 
+import com.digitalpersona.uareu.Fmd
+import com.digitalpersona.uareu.Reader
+
 import griffon.core.artifact.GriffonModel
 import griffon.transform.FXObservable
 import griffon.metadata.ArtifactProviderFor
 
 @ArtifactProviderFor(GriffonModel)
 class GriffonFingerprintModel {
-    @FXObservable String clickCount = "0"
+
+    // Lector de huellas asociado (null si no se detectó ninguno)
+    Reader                  reader
+    // Última huella capturada (null en caso de error)
+    Reader.CaptureResult    captureResult
+    // Última huella enrolada (null en caso de error)
+    Fmd                     enrollmentFMD
+    // Mesajes de estado de la aplicación
+    @FXObservable String    estado = ""
 }
